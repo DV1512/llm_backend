bc00dc2e514f6baf9da4660b8daf19aba78703c2
writemorecode <gustav.ae.karlsson@gmail.com>
2024-11-09 20:55:34 +0100

    First commit: Can stream LLM responses using Kalosm and Actix-Web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::web::Bytes;
+use actix_web::{middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
+use env_logger::Env;
+use kalosm::language::{Chat, Llama, LlamaSource};
+use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
+use tokio_stream::StreamExt;
+
+type StreamItem = Result<Bytes, actix_web::Error>;
+
+#[derive(Serialize, Deserialize)]
+struct ChatRequest {
+    prompt: String,
+}
+
+#[post("/stream")]
+async fn stream_endpoint(
+    web::Json(request_data): web::Json<ChatRequest>,
+    state: web::Data<AppState>,
+) -> impl Responder {
+    let stream = state.chat.lock().unwrap().add_message(request_data.prompt);
+    HttpResponse::Ok().streaming(stream.map(|item| -> StreamItem { Ok(Bytes::from(item)) }))
+}
+
+struct AppState {
+    chat: Mutex<Chat>,
+}
+
+impl AppState {
+    async fn new() -> Self {
+        let model = Llama::builder()
+            .with_source(LlamaSource::llama_3_2_3b_chat())
+            .build()
+            .await
+            .unwrap();
+        Self {
+            chat: Mutex::new(Chat::builder(model).build()),
+        }
+    }
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    let state = web::Data::new(AppState::new().await);
+
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .app_data(state.clone())
+            .service(stream_endpoint)
+    })
+    .bind("[::1]:8000")?
+    .run()
+    .await
 }
