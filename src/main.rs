3b0549cf2299a5dc921199a61942466e12b42ed8
writemorecode <gustav.ae.karlsson@gmail.com>
2024-11-13 20:27:04 +0100

    Now sending JSON containing timestamp and ULID with each chunk

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use kalosm::language::{Chat, Llama, LlamaSource};
 use serde::{Deserialize, Serialize};
 use std::convert::Infallible;
 use std::sync::Mutex;
+use std::time::{SystemTime, UNIX_EPOCH};
 use tokio_stream::StreamExt;
+use ulid::Ulid;
 
 #[derive(Serialize, Deserialize)]
 struct ChatRequest {
@@ -18,11 +20,53 @@ async fn stream_endpoint(
     state: web::Data<AppState>,
 ) -> impl Responder {
     let stream = state.chat.lock().unwrap().add_message(request_data.prompt);
+
+    let ulid = Ulid::new();
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards?")
+        .as_secs();
+
     let sse_stream = stream.map(move |item| -> Result<sse::Event, _> {
-        let data = sse::Data::new(item);
+        let chunk = ChatMessageChunk::new(ulid, timestamp, ChatRole::User, item);
+        let chunk_str = serde_json::to_string(&chunk).unwrap();
+        let data = sse::Data::new(chunk_str);
         Ok::<_, Infallible>(sse::Event::Data(data))
     });
     sse::Sse::from_stream(sse_stream)
+}
+
+#[derive(Serialize)]
+enum ChatRole {
+    System,
+    User,
+    Assistant,
+}
+
+/// A single chunk of a chat message.
+#[derive(Serialize)]
+struct ChatMessageChunk {
+    /// A unique identifier for the chat completion.
+    /// Each chunk has the same ID.
+    id: Ulid,
+    /// The Unix timestamp (in seconds) of when the chat completion was created.
+    /// Each chunk has the same timestamp.
+    timestamp: u64,
+    /// The role of the author of this message.
+    role: ChatRole,
+    /// The contents of the chunk message.
+    contents: String,
+}
+
+impl ChatMessageChunk {
+    fn new(id: ulid::Ulid, timestamp: u64, role: ChatRole, contents: String) -> Self {
+        Self {
+            id,
+            timestamp,
+            role,
+            contents,
+        }
+    }
 }
 
 struct AppState {
